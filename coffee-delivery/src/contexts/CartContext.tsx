516f47eb01c6826028e55f08dc8fb9ea4fe5a43e
Tiago Costa <tiagodpv@gmail.com>
2022-10-17 19:58:44 -0400

    add context

---
+++ coffee-delivery/src/contexts/CartContext.tsx
@@ -0,0 +1,25 @@
+import { createContext, ReactNode, useState } from "react";
+import { Coffee } from "../pages/Home/components/CoffeeCard";
+
+export interface CartItem extends Coffee {
+  quantity: number;
+}
+
+interface CartContextType {
+  cartItems: CartItem[];
+}
+
+interface CartContextProviderProps{
+  children:ReactNode;
+}
+export const CartContext = createContext({ } as CartContextType);
+
+export function CartContextProvider({children}: CartContextProviderProps){
+  const [cartItems, setCartItems] = useState<CartItem[]>([]);
+
+  return(
+    <CartContext.Provider value={{cartItems}}>
+
+    </CartContext.Provider>
+  )
+}
