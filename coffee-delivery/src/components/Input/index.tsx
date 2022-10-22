ea96e74e3a8510ef500e3929ca16ed56cf817566
Tiago Costa <tiagodpv@gmail.com>
2022-10-21 23:00:13 -0400

    form validation

--- coffee-delivery/src/components/Input/index.tsx
+++ coffee-delivery/src/components/Input/index.tsx
@@ -1,10 +1,23 @@
-import { InputHTMLAttributes } from "react";
-import { InputStyledContainer } from "./styles";
+import { forwardRef, InputHTMLAttributes } from "react";
+import { RegularText } from "../Typography";
+import { InputStyleContainer, InputStyled, InputWrapper, RigthText } from "./styles";
 
-type InputProps = InputHTMLAttributes<HTMLInputElement>;
+type InputProps = InputHTMLAttributes<HTMLInputElement> & {
+  error?: string;
+  rightText?: string;
+};
 
-export function Input({...props}: InputProps){
-  return(
-    <InputStyledContainer {...props}/>
-  )
-}
+
+
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({error, rightText, className,...props}, ref) => {
+    return(
+      <InputWrapper className={className}>
+        <InputStyleContainer>
+          <InputStyled {...props} ref={ref}/> 
+          {rightText && <RigthText>{rightText}</RigthText>}
+        </InputStyleContainer>       
+        {error && <RegularText size="s">{error}</RegularText>}
+      </InputWrapper>      
+    );
+  });
